416ce28d073ba75d0f7e61d72bfa5c3bd32fabe2
prattvaidya <pratt.vaidya@gmail.com>
2021-02-26 15:13:37 -0800

    useState to increment and decrement goalBooks

--- todomvc-01-07-start/src/components/Footer.js
+++ todomvc-01-07-start/src/components/Footer.js
@@ -1,30 +1,47 @@
-import React from 'react'
-import PropTypes from 'prop-types'
-import { SHOW_ALL, SHOW_COMPLETED, SHOW_ACTIVE } from '../constants/TodoFilters'
+import React, { useState } from "react";
+import PropTypes from "prop-types";
+import {
+  SHOW_ALL,
+  SHOW_COMPLETED,
+  SHOW_ACTIVE,
+} from "../constants/TodoFilters";
 
 const FILTER_TITLES = {
-  [SHOW_ALL]: 'All',
-  [SHOW_ACTIVE]: 'Active',
-  [SHOW_COMPLETED]: 'Completed'
-}
+  [SHOW_ALL]: "All",
+  [SHOW_ACTIVE]: "Active",
+  [SHOW_COMPLETED]: "Completed",
+};
 
 const Footer = (props) => {
-  const { activeCount } = props
-  const itemWord = activeCount === 1 ? 'book' : 'books'
+  const { activeCount } = props;
+  const itemWord = activeCount === 1 ? "book" : "books";
+  const [goalBooks, setGoalBooks] = useState(0);
   return (
     <footer className="footer">
       <span className="todo-count">
-        <strong>{activeCount || 'No'}</strong> {itemWord} left to read
+        <strong>{activeCount || "No"}</strong> {itemWord} left to read out of{" "}
+        {goalBooks} books
       </span>
-    
+      <button
+        onClick={() => setGoalBooks(goalBooks + 1)}
+        style={{ textSize: "14px", margin: "5px", color: "blue" }}
+      >
+        Increment
+      </button>
+      <button
+        onClick={() => setGoalBooks(goalBooks - 1)}
+        style={{ textSize: "14px", margin: "5px", color: "blue" }}
+      >
+        Decrement
+      </button>
     </footer>
-  )
-}
+  );
+};
 
 Footer.propTypes = {
   completedCount: PropTypes.number.isRequired,
   activeCount: PropTypes.number.isRequired,
   onClearCompleted: PropTypes.func.isRequired,
-}
+};
 
-export default Footer
+export default Footer;
--- todomvc-01-07-start/src/components/MainSection.js
+++ todomvc-01-07-start/src/components/MainSection.js
@@ -1,11 +1,10 @@
-import React, {Component} from 'react'
-import PropTypes from 'prop-types'
-import Footer from './Footer'
-import VisibleBookList from '../containers/VisibleBookList'
-import {BookProvider} from '../context/BookContext';
+import React, { Component } from "react";
+import PropTypes from "prop-types";
+import Footer from "./Footer";
+import VisibleBookList from "../containers/VisibleBookList";
+import { BookProvider } from "../context/BookContext";
 
-import classicBooks from '../constants/books'
-
+import classicBooks from "../constants/books";
 
 class MainSection extends Component {
   constructor(props) {
@@ -19,33 +18,47 @@ class MainSection extends Component {
   }
 
   componentWillMount() {
-    fetch("https://www.googleapis.com/books/v1/volumes?q=inauthor:jane%20austen")
-    .then(response => response.json())
-    .then(myData => this.setState({books: myData.items}))
+    fetch(
+      "https://www.googleapis.com/books/v1/volumes?q=inauthor:jane%20austen"
+    )
+      .then((response) => response.json())
+      .then((myData) => this.setState({ books: myData.items }));
   }
 
   updateBookStatus(bookId, status) {
     let newBooks = this.state.books;
-    const book = newBooks.find(book => book.id === bookId)
+    const book = newBooks.find((book) => book.id === bookId);
     book.status = status;
     this.setState({
       books: newBooks,
-      completedBooks: status === 'completed' ? 
-        this.state.completedBooks + 1 : 
-        this.state.completedBooks - 1,
-    })
+      completedBooks:
+        status === "completed"
+          ? this.state.completedBooks + 1
+          : this.state.completedBooks - 1,
+    });
   }
 
   render() {
     return (
-      <BookProvider value={{showAuthors: this.state.showAuthors}}>
+      <BookProvider value={{ showAuthors: this.state.showAuthors }}>
         <section className="main">
-        <button onClick={() => this.setState({showAuthors: !this.state.showAuthors})} style={{textSize: '14px', margin: '5px', color: 'blue'}}> Toggle authors </button>
-          <VisibleBookList books={this.state.books} updateBookStatus={this.updateBookStatus} />
-          <Footer  
+          <Footer
             completedCount={this.state.completedBooks}
             activeCount={this.state.books.length - this.state.completedBooks}
             onClearCompleted={this.props.actions.clearCompleted}
+          />
+          <button
+            onClick={() =>
+              this.setState({ showAuthors: !this.state.showAuthors })
+            }
+            style={{ textSize: "14px", margin: "5px", color: "blue" }}
+          >
+            {" "}
+            Toggle authors{" "}
+          </button>
+          <VisibleBookList
+            books={this.state.books}
+            updateBookStatus={this.updateBookStatus}
           />
         </section>
       </BookProvider>
@@ -54,15 +67,11 @@ class MainSection extends Component {
 }
 
 MainSection.propTypes = {
-  actions: PropTypes.object.isRequired
-}
+  actions: PropTypes.object.isRequired,
+};
 
 export default MainSection;
 
-
-
-
-
-  /*
+/*
     "https://www.googleapis.com/books/v1/volumes?q=inauthor:jane%20austen"
   */
