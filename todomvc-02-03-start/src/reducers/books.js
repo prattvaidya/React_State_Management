a53ea0e1fe557b12e042f0409aaae5151f54bce4
prattvaidya <pratt.vaidya@gmail.com>
2021-02-26 15:18:42 -0800

    Redux: UpdateBook action and reducer

--- todomvc-02-03-start/src/reducers/books.js
+++ todomvc-02-03-start/src/reducers/books.js
@@ -4,20 +4,27 @@ import {
   EDIT_TODO,
   COMPLETE_TODO,
   COMPLETE_ALL_TODOS,
-  CLEAR_COMPLETED
-} from '../constants/ActionTypes'
+  CLEAR_COMPLETED,
+  UPDATE_BOOK,
+} from "../constants/ActionTypes";
 
-import classicBooks from '../constants/books';
+import classicBooks from "../constants/books";
 
 const initialState = {
   books: classicBooks,
-  completedBooks: 0
-}
-   
+  completedBooks: 0,
+};
 
 export default function todos(state = initialState, action) {
   switch (action.type) {
+    case UPDATE_BOOK:
+      return {
+        ...state,
+        books: state.books.map((book) =>
+          book.id === action.id ? { ...book, completed: !book.completed } : book
+        ),
+      };
     default:
-      return state
+      return state;
   }
 }
